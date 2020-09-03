8bd298bd08cf4ac09f73707b8cac11d097d7d793
arbrown110 <arbrown110@gmail.com>
2020-09-03 18:49:49 -0400

    routes & models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :profiles
+    has_many :contacts
+    has_many :contacted_pages, through :contacts
+    has_many :contacts, through :pages
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  resources :contacts
+  get '/' => 'session#index'
+  get '/login' => 'session#new'
+  get '/signup', to: 'users#new', as: 'register'
+  post '/signup' => 'users#create'
+
+
   resources :users
+  resources :contacts
+  
+  root to: 'user#new'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
