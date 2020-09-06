23913bc8e47da8ab4956f5d8237f7385d28b7ac4
arbrown110 <arbrown110@gmail.com>
2020-09-05 21:10:40 -0400

    update to controller , views and helpers.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,17 @@ class SessionsController < ApplicationController
      redirect_to root_path
     end
 
+    def create
+     user = User.find_by(username: params[:user][:username])
+     if user && user.authentication(params[:user][:username])
+         session[:user_id] = @user.id
+         redirect_to user_path(user)
+     else
+         flash[:message] = "Wrong login, try agan"
+         redirect_to login_path
+     end    
+    end
+
     def new
     end
 
