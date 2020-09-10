7b0c5722c634c7610419d782b42bc05c5bcbd82e
arbrown110 <arbrown110@gmail.com>
2020-09-09 22:50:30 -0400

    adding validation .

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,4 +4,12 @@ class Page < ApplicationRecord
   has_many :users, through: :contacts
 
   validates :name, :information, :photo, prescence: true
+  validate :pages_limit, on: :create
+
+  def pages_with_limits
+    if self.user.pages(:reload).count >= self.user.pages_limit 
+      errors.add(:base, 'Exceeded pages limit')
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
     has_many :contacts
     has_many :contacted_pages, through: :contacts, source: :pages
     has_secure_password 
+
+
+
+    def pages_limit
+        roles.first.pages.count
+      end
+    end
 end
